58291ded3ac9a92a5913129cc8ee321bd1fc35b8
bonpapajosie <35196773+bonpapajosie@users.noreply.github.com>
2019-11-23 22:16:24 +0100

    atom lib added

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -8,6 +8,9 @@ wget https://raw.githubusercontent.com/bonpapajosie/fedorasetup/master/bashrc.al
 sudo dnf install fedora-workstation-repositories -y
 # Install Rpmfusion repo
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+# Install Atom repo
+sudo rpm --import https://packagecloud.io/AtomEditor/atom/gpgkey
+sudo sh -c 'echo -e "[Atom]\nname=Atom Editor\nbaseurl=https://packagecloud.io/AtomEditor/atom/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/AtomEditor/atom/gpgkey" > /etc/yum.repos.d/atom.repo'
 # Update cache for package installs
 sudo dnf makecache
 # Install Dropbox
